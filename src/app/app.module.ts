c810223d8a2546cd02e5014a2aa2ca6e09d3509f
Shane McNeil <shanemcneil19@gmail.com>
2020-08-18 08:58:35 -0700

    add sign-in-page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { SignInPageComponent } from './sign-in-page/sign-in-page.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
