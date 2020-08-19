91d6ab52ebbea2addc58ce0f063920b166d18a24
Shane McNeil <shanemcneil19@gmail.com>
2020-08-18 20:02:45 -0700

    build categories service

--- src/app/services/categories.service.ts
+++ src/app/services/categories.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+const baseUrl = 'https://geekbox.herokuapp.com/api/categories';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoriesService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<any> {
+    return this.http.get(baseUrl);
+  }
+
+  get(id): Observable<any> {
+    return this.http.get(`${baseUrl}/${id}`);
+  }
+
+  create(data): Observable<any> {
+    return this.http.post(baseUrl, data);
+  }
+
+  update(id, data): Observable<any> {
+    return this.http.put(`${baseUrl}/${IDBTransaction}`, data);
+  }
+
+  delete(id): Observable<any> {
+    return this.http.delete(`${baseUrl}/${id}`);
+  }
 }
